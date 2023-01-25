a5a573ffece86514724c07a6ac2c36850e098010
PetarHarambasa <croman.3365@gmail.com>
2023-01-25 11:38:01 +0100

    implemented fix

--- app/src/main/java/hr/algebra/ark/api/ArkApi.kt
+++ app/src/main/java/hr/algebra/ark/api/ArkApi.kt
@@ -3,7 +3,8 @@ package hr.algebra.ark.api
 import retrofit2.Call
 import retrofit2.http.GET
 
-const val API_URL = "https://53e6-94-250-167-253.eu.ngrok.io/"
+//change it every time you start new ngrok forwarding api
+const val API_URL = "https://fd03-94-250-167-253.eu.ngrok.io/"
 
 interface ArkApi {
     @GET("arkCreatures")
